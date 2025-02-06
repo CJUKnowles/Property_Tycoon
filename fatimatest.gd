fed3f5020b329466f7de140b6a1b6addbf83f5cb
ftma48 <falaraji05@gmail.com>
2025-02-06 14:13:36 +0000

    fatima test

---
+++ fatimatest.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+@export var moveSpeed : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var movement_input = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		movement_input.x += 1
+	if Input.is_action_pressed("ui_left"):
+		movement_input.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		movement_input.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		movement_input.y += 1
+		
+	position += movement_input* moveSpeed
+	pass
