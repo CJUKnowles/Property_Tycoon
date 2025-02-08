9e18b27630d5bfd981bc524ef43879517787c5fe
ftma48 <falaraji05@gmail.com>
2025-02-08 14:16:48 +0000

    Update Board.gd

--- Board.gd
+++ Board.gd
@@ -1,3 +1,11 @@
 extends Resource
 
 class_name Board
+
+const head: Space = null
+
+func addSpace(new_space: Space):
+	var current = head
+	while current.next != null:
+		current = current.next 
+		current.next = Space
