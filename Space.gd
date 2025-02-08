25b6f7c45fd42b80067cd46007b31bf01c459ca9
Jin <jxchan14@gmail.com>
2025-02-08 13:41:24 +0000

    space class
    added space class

---
+++ Space.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name Space
+
+enum SpaceType {
+	PROPERTY,
+	POT_LUCK,
+	OPPORTUNITY_KNOCK,
+	JAIL,
+	FREE_PARKING,
+	GO_TO_JAIL,
+	TAX,
+	GO
+}
+
+@export var name: String
+@export var type: SpaceType
+@export var property: Property = null
+@export var playersOnSpace: Array = []
