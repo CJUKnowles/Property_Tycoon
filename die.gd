84ac5c1116797a6f9e7632e578df2a6c933b4588
ftma48 <falaraji05@gmail.com>
2025-02-09 20:00:55 +0000

    dice

---
+++ die.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func roll():
+	var die1 = randi_range(1,6)
+	var die2 = randi_range(1,6)
+	#code for visuals
+	return(die1+die2)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_select"):
+		print(roll())
+	pass
