8cc396ab5b306cc37edf3526dbb664d2df6fc056
techgeniusx <deme3.1010@gmail.com>
2025-02-16 20:43:48 +0000

    Coded Monopoly Space Thing
    Gonna add each individual property soon

---
+++ Scripts/board_space.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var space_name: String = "Go"
+var price: int = 0
+var rent: int = 0
+var owner: Node = null  # who the owner is
+
+func on_land(player):
+	print(player.name, " just landed on: ", space_name)
+	# thing that happens when player lands
