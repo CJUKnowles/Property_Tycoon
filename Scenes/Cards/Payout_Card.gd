b1f0c51109072d80dc58ab07883ad316a005e646
ftma48 <falaraji05@gmail.com>
2025-03-01 14:11:50 +0000

    Update Payout_Card.gd

--- Scenes/Cards/Payout_Card.gd
+++ Scenes/Cards/Payout_Card.gd
@@ -2,9 +2,11 @@ extends Card
 
 var value: int # payout amount
 var target: String # target of payout
+var bank: Bank
 
 func on_draw():
 	var player = gameManager.get_current_player()
-
-		
+	player.money += value
+	if target == "Bank":
+		bank.receive_payment(value)
 	pass
