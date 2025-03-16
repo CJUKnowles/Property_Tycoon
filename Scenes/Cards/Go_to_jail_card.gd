fb8b00d76a7d9935beaf69df5de03bd0be8494d4
ftma48 <falaraji05@gmail.com>
2025-03-16 23:41:09 +0000

    Update Go_to_jail_card.gd

--- Scenes/Cards/Go_to_jail_card.gd
+++ Scenes/Cards/Go_to_jail_card.gd
@@ -2,17 +2,7 @@ extends Card
 class_name Go_to_jail_card
 
 var player = gameManager.get_current_player()
-var inDeck : bool = true
+
 # Called when the node enters the scene tree for the first time.
 func on_draw():
-	player.getOutOfJailFreeCard = true
-	inDeck = false
-	
-func use():
-	if player.inJail == true:
-		print("You have used your 'get out of jail free card' ! ")
-		player.exitJail()
-		player.getOutOfJailFreeCard = false
-		inDeck = true
-	else:
-		print("You are not in Jail")
+	player.goToJail()
