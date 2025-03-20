7986dc651bf6fad33de9874aef26f7e3004a2924
James Knowles <CJUKnowles@gmail.com>
2025-03-20 22:29:18 +0000

    Improve board scaling

--- Scenes/Game/board_scaler.gd
+++ Scenes/Game/board_scaler.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
-@export var scale_ratio:float
+@export var x_scale_ratio:float
+@export var y_scale_ratio:float
 
 func _process(delta: float) -> void:
 	var window_size = get_viewport().get_visible_rect().size
-	scale = Vector2(window_size.y * scale_ratio * .01, window_size.y * scale_ratio * .01)
+	
+	# determines whether the board scale should depend on the window width or height
+	var new_scale = minf(window_size.y * y_scale_ratio, window_size.x * x_scale_ratio) *.01
+	scale = Vector2(new_scale, new_scale)
