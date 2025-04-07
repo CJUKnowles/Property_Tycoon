4089e81a13a1af42f7dbfb84f8256f03bc06cb7e
CJUKnowles <CJUKnowles@gmail.com>
2025-04-07 19:55:51 +0100

    Delete unncessary files

--- Scenes/Game/Camera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func _process(delta:float):
-	if (Input.is_action_just_pressed("ui_text_scroll_up")):
-		zoom += Vector2(.1,.1)
-	if (Input.is_action_just_pressed("ui_text_scroll_down")):
-		zoom -= Vector2(.1,.1)
