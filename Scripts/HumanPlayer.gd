85c25d39b55255cc15413c1c1fe693920e7c11b6
Jin <jxchan14@gmail.com>
2025-04-10 13:43:46 +0100

    fixed free parking

--- Scripts/HumanPlayer.gd
+++ Scripts/HumanPlayer.gd
@@ -11,52 +11,55 @@ func takeTurn():
 	#var player = gameManager.get_current_player()
 	#var target_space = gameManager.board.findSpace(target)
 	#player.moveTo(target_space)
+	
+	
+	self.fine(0)
 
 	#
 	#collectFromGO = false
 ###
-	if bankrupt:
-		print("skipping turn, player is bankrupt")
-		return
-	if inJail:
-		if jailTurns == 2:
-			exitJail()
-		else:
-			print(name, " is in jail.")
-			print("Rolling to get out of jail.....")
-			rollResult = Die.roll()
-			var die1 = rollResult[0]
-			var die2 = rollResult[1]
-			print("Dice 1: ",die1,", Dice 2: ", die2)
-			if die1 == die2:
-				print("Rolled doubles! you can leave jail")
-				exitJail()
-			else:
-				print("Uh oh! you can't leave yet")
-				jailTurns += 1
-				print("jail time: ", str(jailTurns), "/2")
-	if turn_over:
-		print(name + " is out of rolls.")
-		return
-	if !inJail:
-		print("start money: " , money)
-		print("jail: ",inJail)
-		rollResult = Die.roll()
-		var die1 = rollResult[0]
-		var die2 = rollResult[1]
-		var total = rollResult[2]
-		print("die1: ",die1,", die2: ",die2,", total: ",total)
-		
-		if die1 == die2: # We rolled a double
-			doubleCount += 1
-			print(name, " rolled a double and gets another turn!")
-			if doubleCount == 3:
-				doubleCount = 0 #reset count
-				print(name, " rolled three doubles and has to go to jail!")
-				goToJail()
-			else: 
-				move(total)
-		else: # We did not roll a double
-			move(total)
-			doubleCount = 0 #reset count
-		print("end money: " , money)
+	#if bankrupt:
+		#print("skipping turn, player is bankrupt")
+		#return
+	#if inJail:
+		#if jailTurns == 2:
+			#exitJail()
+		#else:
+			#print(name, " is in jail.")
+			#print("Rolling to get out of jail.....")
+			#rollResult = Die.roll()
+			#var die1 = rollResult[0]
+			#var die2 = rollResult[1]
+			#print("Dice 1: ",die1,", Dice 2: ", die2)
+			#if die1 == die2:
+				#print("Rolled doubles! you can leave jail")
+				#exitJail()
+			#else:
+				#print("Uh oh! you can't leave yet")
+				#jailTurns += 1
+				#print("jail time: ", str(jailTurns), "/2")
+	#if turn_over:
+		#print(name + " is out of rolls.")
+		#return
+	#if !inJail:
+		#print("start money: " , money)
+		#print("jail: ",inJail)
+		#rollResult = Die.roll()
+		#var die1 = rollResult[0]
+		#var die2 = rollResult[1]
+		#var total = rollResult[2]
+		#print("die1: ",die1,", die2: ",die2,", total: ",total)
+		#
+		#if die1 == die2: # We rolled a double
+			#doubleCount += 1
+			#print(name, " rolled a double and gets another turn!")
+			#if doubleCount == 3:
+				#doubleCount = 0 #reset count
+				#print(name, " rolled three doubles and has to go to jail!")
+				#goToJail()
+			#else: 
+				#move(total)
+		#else: # We did not roll a double
+			#move(total)
+			#doubleCount = 0 #reset count
+		#print("end money: " , money)
